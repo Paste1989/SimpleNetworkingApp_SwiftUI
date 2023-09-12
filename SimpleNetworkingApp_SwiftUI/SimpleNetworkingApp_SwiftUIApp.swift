2b96b288b9eb58f55843989291a29083ae3d68bb
Sasa89 <sasa.brezovac@cobeisfresh.com>
2023-09-12 10:51:28 +0200

    Initial Commit

---
+++ SimpleNetworkingApp_SwiftUI/SimpleNetworkingApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNetworkingApp_SwiftUIApp.swift
+//  SimpleNetworkingApp_SwiftUI
+//
+//  Created by Saša Brezovac on 12.09.2023..
+//
+
+import SwiftUI
+
+@main
+struct SimpleNetworkingApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
